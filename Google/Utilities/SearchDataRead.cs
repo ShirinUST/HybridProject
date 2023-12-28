946cd9a43b26b2be197bd217411e9fba95d7c296
ShirinUST <shirin.safwanashiyas@ust.com>
2023-12-28 12:12:25 +0530

    Add project files.

---
+++ Google/Utilities/SearchDataRead.cs
@@ -0,0 +1,56 @@
+﻿using ExcelDataReader;
+using Google.Test_Data_Classes;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Google.Utilities
+{
+    public class SearchDataRead
+    {
+        public static List<SearchData> ReadSearchText(string excelFilePath,string sheetname)
+        {
+            List<SearchData> excelDataList = new List<SearchData>();
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            using (var stream = File.Open(excelFilePath, FileMode.Open, FileAccess.Read))
+            {
+                using (var reader = ExcelReaderFactory.CreateReader(stream))
+                {
+                    var result = reader.AsDataSet(new ExcelDataSetConfiguration()
+                    {
+                        ConfigureDataTable = (_) => new ExcelDataTableConfiguration()
+                        {
+                            UseHeaderRow = true,
+                        }
+                    });
+                    var dataTable = result.Tables[sheetname];
+                    if (dataTable != null)
+                    {
+                        foreach (DataRow row in dataTable.Rows)
+                        {
+                            SearchData excelData = new()
+                            {
+                                SearchText = GetValueOrDefault(row, "searchtext"),
+                            };
+                            excelDataList.Add(excelData);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Sheet '{sheetname}' not found in the Excel file.");
+                    }
+                }
+            }
+            return excelDataList;
+        }
+        static string? GetValueOrDefault(DataRow row, string columnName)
+        {
+            Console.WriteLine(row + "  " + columnName);
+            return row.Table.Columns.Contains(columnName) ? row[columnName]?.ToString() : null;
+        }
+    }
+}
