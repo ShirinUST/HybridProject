6108aa81e2f9b177d442bcb74326104f5df54c38
249259 <249259@L597LTRV>
2023-12-28 12:26:40 +0530

    commit

--- Google/Selenium Tests/Pages/BasePage.cs
+++ Google/Selenium Tests/Pages/BasePage.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using AventStack.ExtentReports.Reporter;
+using AventStack.ExtentReports;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium;
+using Serilog;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +14,125 @@ namespace Google.Selenium_Tests.Pages
 {
     internal class BasePage
     {
+        protected IWebDriver Driver;
+        protected ExtentReports Extent;
+        protected ExtentSparkReporter SparkReporter;
+        protected ExtentTest Test;
+
+        private Dictionary<string, string> Properties;
+        protected string? currdir;
+        protected string url;
+
+        //overloaded constructors
+        protected BasePage()
+        {
+            currdir = Directory.GetParent(@"../../../")?.FullName;
+        }
+        public BasePage(IWebDriver driver)
+        {
+            Driver = driver;
+        }
+
+        public void ReadConfigSettings()
+        {
+            string currentDir = Directory.GetParent(@"../../../").FullName;
+
+            string fullPath = currentDir + "/configsettings/config.properties";
+
+            string[] lines = File.ReadAllLines(fullPath);
+            Properties = new Dictionary<string, string>();
+            foreach (string line in lines)
+            {
+                if (!string.IsNullOrEmpty(line) && line.Contains('='))
+                {
+                    string[] split = line.Split('=');
+                    string key = split[0].Trim();
+                    string value = split[1].Trim();
+                    Properties[key] = value;
+                }
+            }
+
+        }
+
+        protected static void ScrollViewInto(IWebDriver driver, IWebElement element)
+        {
+            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+            js.ExecuteScript("arguments[0].scrollIntoView(true)", element);
+        }
+        protected void TakeScreenShot(string testName)
+        {
+            ITakesScreenshot screenshot = (ITakesScreenshot)Driver;
+            Screenshot ss = screenshot.GetScreenshot();
+            string currdir = Directory.GetParent(@"../../../").FullName;
+            string filepath = currdir + "/Screenshots/" + testName + "scs_" + DateTime.Now.ToString("yyyyMMdd_HHmmss") + ".png";
+            ss.SaveAsFile(filepath);
+
+        }
+        protected void LogTestResult(string testName, string type, string result, string errorMessage = null)
+        {
+            if (type.ToLower().Equals("info"))
+            {
+                Log.Information(result);
+                Test?.Info(result);
+            }
+            else if (type.ToLower().Equals("pass") && errorMessage == null)
+            {
+                Log.Information(testName + "passed");
+                Log.Information(".....................................");
+                Test?.Pass(result);
+            }
+            else
+            {
+                Log.Error($"Test failed for {testName} .\n Exception: \n {errorMessage}");
+                Log.Information("........................................");
+                var screenshot = ((ITakesScreenshot)Driver).GetScreenshot().AsBase64EncodedString;
+                Test?.AddScreenCaptureFromBase64String(screenshot);
+                Test?.Fail(result);
+
+            }
+
+        }
+        protected void InitializeBrowser()
+        {
+            ReadConfigSettings();
+            if (Properties["﻿browser"].ToLower() == "edge")
+            {
+                Driver = new EdgeDriver();
+            }
+            else if (Properties["﻿browser"].ToLower() == "chrome")
+            {
+                Driver = new ChromeDriver();
+            }
+
+            url = Properties["baseUrl"];
+            Driver.Url = url;
+            Driver.Manage().Window.Maximize();
+
+        }
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            InitializeBrowser();
+
+            string directory = Directory.GetParent(@"../../../").FullName;
+            string logfilepath = directory + "/Logs/log_" + DateTime.Now.ToString("yyyyMMdd_HHmmss") + ".txt";
+            Log.Logger = new LoggerConfiguration().
+                    WriteTo.Console().
+                    WriteTo.File(logfilepath, rollingInterval: RollingInterval.Day).
+                    CreateLogger();
+            string currentDir = Directory.GetParent(@"../../../").FullName;
+            Extent = new ExtentReports();
+            SparkReporter = new ExtentSparkReporter(currentDir + "/Reports/report_" + DateTime.Now.ToString("yyyyMMdd_HHmmss") + ".html");
+            Extent.AttachReporter(SparkReporter);
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            Extent.Flush();
+            Driver.Quit();
+            Log.CloseAndFlush();
+        }
     }
 }
---
+++ Google/Selenium Tests/Tests/HomePageTests.cs
@@ -0,0 +1,83 @@
+﻿using Google.Selenium_Tests.Pages;
+using Google.Utilities;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Google.Selenium_Tests.Tests
+{
+    internal class HomePageTests:BasePage
+    {
+        private HomePage homepage;
+        string? testName;
+        public void BeforeTest()
+        {
+            homepage = new HomePage(Driver);
+        }
+        [Test]
+        public void GoogleSearchTest()
+        {
+
+            testName = "Google Search Test";
+            Log.Information(testName);
+            Log.Information("************************************************");
+            Test = Extent.CreateTest(testName);
+
+
+            string? excelFilePath = currdir + "/Test Data/Google Data.xlsx";
+            string? sheetName = "Search Data";
+
+            List<GoogleSearchText> excelDataList = SearchDataRead.ReadSearchText(excelFilePath, sheetName);
+
+            foreach (var excelData in excelDataList)
+            {
+                string? searchText = excelData.SearchText;
+
+
+                LogTestResult(testName, "Info", "Starting the Google search test");
+
+                LogTestResult(testName, "Info", "Opened Google homepage");
+
+                homePage.EnterSearchText(searchText);
+                Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(2);
+                TakeScreenShot();
+                LogTestResult(testName, "Info", $"Entered search text: {searchText}");
+
+                homePage.ClickSearchButton();
+                Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(2);
+                TakeScreenShot();
+                LogTestResult(testName, "Info", "Clicked on the Search button");
+
+                try
+                {
+
+                    // Assume the title contains the search text for simplicity
+                    Assert.That(Driver.Title.Contains(searchText), Is.True, $"Search results page title does not contain '{searchText}'");
+                    LogTestResult(testName, "Info", "Google search test completed");
+                    LogTestResult(testName, "pass", testName + " - Passed");
+                }
+                catch (Exception ex)
+                {
+                    LogTestResult(testName, "fail", testName + " - Failed", ex.Message);
+                }
+                finally
+                {
+                    Driver.Navigate().GoToUrl(url);
+                }
+                // Additional assertions or actions if needed
+            }
+        }
+
+        [TearDown]
+        public void AfterTest()
+        {
+            Driver.Navigate().GoToUrl(url);
+            Log.Information("************************************************");
+        }
+
+    }
+}
